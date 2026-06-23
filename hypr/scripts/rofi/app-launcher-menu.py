5477143e480b411a90ffc16ff39d038e0d548bfc
tom <tom>
2026-06-23 16:58:02 +0200

    before css simplify

--- hypr/scripts/rofi/app-launcher-menu.py
+++ hypr/scripts/rofi/app-launcher-menu.py
@@ -5,6 +5,8 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from config import ROFI_THEME_LAUNCHER
+
 
 subprocess.run(["rofi", "-show", "drun", "-matching", "fuzzy", "-theme", str(ROFI_THEME_LAUNCHER)])
--- waybar/scripts/toggle_mute_speakers.sh
+++ waybar/scripts/toggle_mute_speakers.sh
@@ -1,4 +1,4 @@
-mapfile -t NODE_NUMBERS < <(wpctl list | grep alsa_output | awk '{print $1}')
+mapfile -t NODE_NUMBERS < <(wpctl list | grep output | awk '{print $1}')
 
 for NODE in "${NODE_NUMBERS[@]}"; do
     wpctl set-mute $NODE toggle
